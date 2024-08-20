77ae7a2e941b4b7d5c306ac1a50ce6fdf8a84a1f
DenisDK <unworkdk@gmail.com>
2024-08-20 15:48:54 +0300

    Sedebar component

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Home = () => {
+const Previous = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      <h1 className="text-3xl font-bold">Welcome to the Previous Page</h1>
     </section>
   );
 };
 
-export default Home;
+export default Previous;
